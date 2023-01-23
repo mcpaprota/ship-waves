68094169f8fbcad30417145bbebc79d615aa1a35
mcpaprota <105712326+mcpaprota@users.noreply.github.com>
2023-01-23 10:06:37 +0100

    Update shipWaves.m

--- shipWaves.m
+++ shipWaves.m
@@ -2,7 +2,7 @@ function [kappa, kappa_i, kappa_j, eta_hat, phi_hat, p_hat] = ...
     shipWaves(NumModel,Vessel,Constants)
 % Fourier Galerkin solution to gravity-capillary linear ship waves
 % Author: Maciej Paprota
-% Reference: M. Paprota. 2022. A Fourier Galerkin method for ship waves
+% Reference: M. Paprota. 2023. A Fourier Galerkin method for ship waves. Ocean Engineering (accepted manuscript)
 % Input data:
 l = NumModel.domainLength;
 b = NumModel.domainBreadth;
@@ -31,24 +31,24 @@ rho = Constants.waterDensity;
 
 % solution eigenvalues
 if isRefl
-    kappa_i = ones(J,1)*(0:I-1)*pi/l; % Eq. (xx)
-    kappa_j = (0:J-1)'*ones(1,I)*pi/b; % Eq. (xx)
+    kappa_i = ones(J,1)*(0:I-1)*pi/l; % Eq. (47)
+    kappa_j = (0:J-1)'*ones(1,I)*pi/b; % Eq. (48)
 else
-    kappa_i = ones(J,1)*(-I/2:I/2-1)*2*pi/l; % Eq. (xx)
-    kappa_j = (-J/2:J/2-1)'*ones(1,I)*2*pi/b; % Eq. (xx)
+    kappa_i = ones(J,1)*(-I/2:I/2-1)*2*pi/l; % Eq. (9)
+    kappa_j = (-J/2:J/2-1)'*ones(1,I)*2*pi/b; % Eq. (10)
 end
-kappa = sqrt(kappa_i.^2+kappa_j.^2); % Eq. (xx)
+kappa = sqrt(kappa_i.^2+kappa_j.^2); % Eq. (11)
 kappatanhkappad = kappa.*tanh(kappa*d);
 omega = sqrt((g+gamma*kappa.*kappa/rho)...
-    .*kappatanhkappad); % Eq. (xx)
+    .*kappatanhkappad); % Eq. (38)
 % moving pressure term
 N = length(X);
 p_hat = zeros(N,J,I);
 p_hat(1,:,:) = movingPressureAmps(kappa_i,kappa_j,l,b,L,B,D,...
     theta(1),X(1),Y(1),X_m0,Y_m0,theta_m0,isRefl)*g./...
     (g+gamma*kappa.^2/rho);
-eta_hat = -p_hat; % initial free-surface elevation coefficients
-phi_hat = zeros(N,J,I); % initial velocity potential coefficients
+phi_hat = zeros(N,J,I); % initial velocity potential coefficients - Eq. (17)
+eta_hat = -p_hat; % initial free-surface elevation coefficients - Eq. (18)
 % Modified Euler implicit scheme loop:
 for n=1:N-1
     p_hat(n+1,:,:) = movingPressureAmps(kappa_i,kappa_j,l,b,L,B,D,...
@@ -58,11 +58,11 @@ for n=1:N-1
         (dt*omega/2).^2.*(squeeze(eta_hat(n,:,:)+...
         p_hat(n+1,:,:)+p_hat(n,:,:)))+...
         dt*squeeze(phi_hat(n,:,:)).*kappatanhkappad)./...
-        (1+(dt*omega/2).^2); % Eq. (32)
+        (1+(dt*omega/2).^2); % Eq. (39)
     phi_hat(n+1,:,:) = ((1-(dt*omega/2).^2).*squeeze(phi_hat(n,:,:))-...
         dt/2.*(g+gamma*kappa.*kappa/rho).*...
         (squeeze(2*eta_hat(n,:,:)+...
-        p_hat(n+1,:,:)+p_hat(n,:,:))))./(1+(dt*omega/2).^2); % Eq. (33);
+        p_hat(n+1,:,:)+p_hat(n,:,:))))./(1+(dt*omega/2).^2); % Eq. (40);
     disp(['Calculation progress: ' num2str((n+1)/N*100,'%6.2f') '%'])
 end
 kappa_i = kappa_i(1,:);

4ec6930f47bbbf7db65d581b4151b95db0787af5
mcpaprota <105712326+mcpaprota@users.noreply.github.com>
2023-01-23 09:55:01 +0100

    Update freeSurfaceElevation.m

--- freeSurfaceElevation.m
+++ freeSurfaceElevation.m
@@ -1,6 +1,7 @@
 function eta = freeSurfaceElevation(eta_hat,kappa_i,kappa_j,x,y,isRefl)
 % calculates free-surface elevation based on amplitudes and eigenvalues of cosine expansion
 % Author: Maciej Paprota
+% Reference: M. Paprota. 2023. A Fourier Galerkin method for ship waves. Ocean Engineering (accepted manuscript)
 % Input data:
 % eta_hat - solution amplitudes (m)
 % kappa_i, kappa_j - solution aigenvalues (1/m)
@@ -8,7 +9,7 @@ function eta = freeSurfaceElevation(eta_hat,kappa_i,kappa_j,x,y,isRefl)
 % Output data:
 % eta - free surface elevation (m)
 if isRefl
-    eta = cos(y'*kappa_j')*eta_hat*cos(kappa_i'*x);
+    eta = cos(y'*kappa_j')*eta_hat*cos(kappa_i'*x); % Eq. (45)
 else
-    eta = real(exp(1i*y'*kappa_j')*eta_hat*exp(1i*kappa_i'*x));
+    eta = real(exp(1i*y'*kappa_j')*eta_hat*exp(1i*kappa_i'*x)); % Eq. (8)
 end

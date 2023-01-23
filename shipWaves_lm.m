b9d832186c977e53134c48e3794bed94088a47e6
mcpaprota <105712326+mcpaprota@users.noreply.github.com>
2023-01-23 10:08:47 +0100

    Update shipWaves_lm.m

--- shipWaves_lm.m
+++ shipWaves_lm.m
@@ -32,23 +32,23 @@ rho = Constants.waterDensity;
 
 % solution eigenvalues
 if isRefl
-    kappa_i = ones(J,1)*(0:I-1)*pi/l; % Eq. (xx)
-    kappa_j = (0:J-1)'*ones(1,I)*pi/b; % Eq. (xx)
+    kappa_i = ones(J,1)*(0:I-1)*pi/l; % Eq. (47)
+    kappa_j = (0:J-1)'*ones(1,I)*pi/b; % Eq. (48)
 else
-    kappa_i = ones(J,1)*(-I/2:I/2-1)*2*pi/l; % Eq. (xx)
-    kappa_j = (-J/2:J/2-1)'*ones(1,I)*2*pi/b; % Eq. (xx)
+    kappa_i = ones(J,1)*(-I/2:I/2-1)*2*pi/l; % Eq. (9)
+    kappa_j = (-J/2:J/2-1)'*ones(1,I)*2*pi/b; % Eq. (10)
 end
-kappa = sqrt(kappa_i.^2+kappa_j.^2); % Eq. (xx)
+kappa = sqrt(kappa_i.^2+kappa_j.^2); % Eq. (11)
 kappatanhkappad = kappa.*tanh(kappa*d);
 omega = sqrt((g+gamma*kappa.*kappa/rho)...
-    .*kappatanhkappad); % Eq. (xx)
+    .*kappatanhkappad); % Eq. (38)
 % moving pressure term
 N = length(X);
 p_hat_p = movingPressureAmps(kappa_i,kappa_j,l,b,L,B,D,...
     theta(1),X(1),Y(1),X_m0,Y_m0,theta_m0,isRefl)*g./...
     (g+gamma*kappa.^2/rho);
-eta_hat_p = -p_hat_p; % initial free-surface elevation coefficients
-phi_hat_p = zeros(J,I); % initial velocity potential coefficients
+phi_hat_p = zeros(J,I); % initial velocity potential coefficients - Eq. (17)
+eta_hat_p = -p_hat_p; % initial free-surface elevation coefficients - Eq. (18)
 % Modified Euler implicit scheme loop:
 for n=1:N-1
     p_hat = movingPressureAmps(kappa_i,kappa_j,l,b,L,B,D,...
@@ -56,10 +56,10 @@ for n=1:N-1
         (g+gamma*kappa.^2/rho);
     eta_hat = (eta_hat_p-(dt*omega/2).^2.*(eta_hat_p+...
         p_hat+p_hat_p)+dt*phi_hat_p.*kappatanhkappad)./...
-        (1+(dt*omega/2).^2); % Eq. (32)
+        (1+(dt*omega/2).^2); % Eq. (39)
     phi_hat = ((1-(dt*omega/2).^2).*phi_hat_p-...
         dt/2.*(g+gamma*kappa.*kappa/rho).*...
-        (2*eta_hat_p+p_hat+p_hat_p))./(1+(dt*omega/2).^2); % Eq. (33);
+        (2*eta_hat_p+p_hat+p_hat_p))./(1+(dt*omega/2).^2); % Eq. (40);
     eta_hat_p = eta_hat; phi_hat_p = phi_hat; p_hat_p = p_hat;
     disp(['Calculation progress: ' num2str((n+1)/N*100,'%6.2f') '%'])
 end

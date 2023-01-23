f72d1a368d2a8350f852e7edb023a3ccef1e3dbf
mcpaprota <105712326+mcpaprota@users.noreply.github.com>
2023-01-23 09:58:54 +0100

    Update movingPressure.m

--- movingPressure.m
+++ movingPressure.m
@@ -1,6 +1,7 @@
 function p = movingPressure(x,y,Vessel,n)
 % moving pressure function
 % Author: Maciej Paprota
+% Reference: M. Paprota. 2023. A Fourier Galerkin method for ship waves. Ocean Engineering (accepted manuscript)
 % Input data:
 L = Vessel.hullLength;
 B = Vessel.hullBreadth;
@@ -16,14 +17,14 @@ y = y'*ones(1,length(x(1,:)));
 p = x*0;
 for m=1:length(X_m0)
     alpha = D(m); 
-    sigma_x = L(m)/sqrt(8*log(10)); % Eq. (xx)
-    sigma_y = B(m)/sqrt(8*log(10)); % Eq. (xx)
-    beta_x = cos(theta+theta_m0(m)).^2/2/sigma_x^2+sin(theta+theta_m0(m)).^2/2/sigma_y^2; % Eq. (xx)
-    beta_xy = sin(2*(theta+theta_m0(m)))/4*(1/sigma_x^2-1/sigma_y^2); % Eq. (xx)
-    beta_y = sin(theta+theta_m0(m)).^2/2/sigma_x^2+cos(theta+theta_m0(m)).^2/2/sigma_y^2; % Eq. (xx)
-    X_m = X_m0(m)*cos(theta)-Y_m0(m)*sin(theta)+X;
-    Y_m = X_m0(m)*sin(theta)+Y_m0(m)*cos(theta)+Y;
+    sigma_x = L(m)/sqrt(8*log(10)); % Eq. (23)
+    sigma_y = B(m)/sqrt(8*log(10)); % Eq. (24)
+    beta_x = cos(theta+theta_m0(m)).^2/2/sigma_x^2+sin(theta+theta_m0(m)).^2/2/sigma_y^2; % Eq. (28)
+    beta_xy = sin(2*(theta+theta_m0(m)))/4*(1/sigma_x^2-1/sigma_y^2); % Eq. (29)
+    beta_y = sin(theta+theta_m0(m)).^2/2/sigma_x^2+cos(theta+theta_m0(m)).^2/2/sigma_y^2; % Eq. (30)
+    X_m = X_m0(m)*cos(theta)-Y_m0(m)*sin(theta)+X; % Eq. (33)
+    Y_m = X_m0(m)*sin(theta)+Y_m0(m)*cos(theta)+Y; % Eq. (34)
     p = p + alpha*exp(-(beta_x*(x-X_m).^2+2*beta_xy*(x-X_m).*(y-Y_m)+...
-        beta_y*(y-Y_m).^2)); % Eqs. (xx) and (xx)
+        beta_y*(y-Y_m).^2)); % Eqs. (31)
 end
 end
